24a617feaf67aad9f36467a9214f9a5428ee591f
chetankhandait <chetankhandait16@gmail.com>
2024-06-12 23:46:25 +0530

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen text-2xl font-bold'>
+      hi gupta gandu
+    </div>
+  )
+}
+
+export default App
