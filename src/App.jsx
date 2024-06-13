4497191b5cb04428d27467cf73c1445f0b8a085e
chetankhandait <chetankhandait16@gmail.com>
2024-06-13 21:26:02 +0530

    testimoninnial

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./Hero";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Services from "./Components/Service";
+import Testimonial from "./Components/Testimonial";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
 
       <Services/>
+      <Testimonial/>
       <Footer/>
     </div>
   );
