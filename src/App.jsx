005b57f97ae341a0bb7d6a14c6896d6f98ef10ac
chiragg987 <cg5031046@gmail.com>
2024-06-13 19:06:29 +0530

    Services

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Hero from "./Hero";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import Services from "./Components/Service";
 
 const App = () => {
   return (
     <div className="">
       {/* <Navbar /> */}
       <Hero />
-      <Footer/>
+      <Services />
+      <Footer />
     </div>
   );
 };
