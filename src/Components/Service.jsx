c04f50d76242aeae3abd441904c1ce2a5acdbebc
chiragg987 <cg5031046@gmail.com>
2024-06-23 03:21:16 +0530

    services

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -1,82 +1,169 @@
+// import React from "react";
+// import {
+//   FaPenNib,
+//   //   FaPaintBrush,
+//   FaBullhorn,
+//   FaLaptopCode,
+//   FaHeadset,
+//   FaFingerprint,
+// } from "react-icons/fa"; // Importing icons
+// import { MdColorLens } from "react-icons/md";
+
+// const services = [
+//   {
+//     title: "Content Marketing",
+//     description:
+//       "Our team creates engaging and shareable content that resonates with your audience, drives organic traffic.",
+//     icon: <FaPenNib />, // using imported icon
+//   },
+//   {
+//     title: "Graphic Design",
+//     description:
+//       "Unlock the power of visual storytelling with our expert graphic design services, tailored to elevate your brand and captivate.",
+//     icon: <MdColorLens />, // using imported icon
+//   },
+//   {
+//     title: "Digital Marketing",
+//     description:
+//       "Elevate your brand’s online presence with our data-driven digital marketing strategies. From SEO and content marketing.",
+//     icon: <FaBullhorn />, // using imported icon
+//   },
+//   {
+//     title: "Web Design",
+//     description:
+//       "We specialize in creating visually stunning, user-friendly websites that align with your brand identity and deliver an exceptional.",
+//     icon: <FaLaptopCode />, // using imported icon
+//   },
+//   {
+//     title: "IT Consulting",
+//     description:
+//       "IT consulting or information technology consulting refers to the practice of providing advisory and implementation services.",
+//     icon: <FaHeadset />, // using imported icon
+//   },
+//   {
+//     title: "Brand Identity",
+//     description:
+//       "It involves creating a unique and recognizable identity that sets the brand apart from competitors and resonates with the target audience.",
+//     icon: <FaFingerprint />, // using imported icon
+//   },
+// ];
+// const Services = () => {
+//   return (
+//     <section className="py-12 bg-white">
+//       <div className="container mx-auto px-6">
+//         <h2 className="text-2xl font-bold text-center text mb-8 text-teal-400">
+//           Our Services
+//         </h2>
+//         <h3 className="text-3xl font-semibold text-center mb-8">
+//           High-impact services for your business
+//         </h3>
+//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+//           {services.map((service, index) => (
+//             <div
+//               key={index}
+//               className="flex flex-col items-left p-6 bg-gray-100 rounded-lg shadow-lg hover:text-white hover:bg-black transition duration-300 cursor-pointer"
+//             >
+//               <div className="text-3xl mb-4">{service.icon}</div>
+//               <h4 className="text-xl  font-semibold mb-2">{service.title}</h4>
+//               <p className="active:text-gray-700 text-left ">
+//                 {service.description}
+//               </p>
+//               {/* <button className="bg-black text-white hover:bg-white hover:text-black py-2 px-4 rounded-full font-semibold mt-4">
+//                 Project Here
+//               </button> */}
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Services;
 import React from "react";
 import {
-  FaPenNib,
-  //   FaPaintBrush,
-  FaBullhorn,
-  FaLaptopCode,
-  FaHeadset,
-  FaFingerprint,
-} from "react-icons/fa"; // Importing icons
-import { MdColorLens } from "react-icons/md";
+  FaCode,
+  FaPencilRuler,
+  FaMobileAlt,
+  FaSearch,
+  FaPalette,
+  FaChartLine,
+} from "react-icons/fa";
 
 const services = [
   {
-    title: "Content Marketing",
+    icon: <FaCode className="text-white" />,
+    title: "Web Development",
     description:
-      "Our team creates engaging and shareable content that resonates with your audience, drives organic traffic.",
-    icon: <FaPenNib />, // using imported icon
+      "Our Business Plan is a written document describing a company's core business activities.",
+    iconBgColor: "bg-yellow-400",
   },
   {
+    icon: <FaPencilRuler className="text-white" />,
+    title: "UI/UX Designer",
+    description:
+      "UI/UX design service provider, our primary goal is to create user-friendly and visually appealing digital products.",
+    iconBgColor: "bg-orange-400",
+  },
+  {
+    icon: <FaMobileAlt className="text-white" />,
+    title: "App Development",
+    description:
+      "UI/UX design service provider, our primary goal is to create user-friendly and visually appealing digital products.",
+    iconBgColor: "bg-blue-400",
+  },
+  {
+    icon: <FaSearch className="text-white" />,
+    title: "Search Engine Optimization",
+    description:
+      "Our Search Engine Optimization (SEO) service is a comprehensive approach to improving your website's visibility and ranking.",
+    iconBgColor: "bg-blue-200",
+  },
+  {
+    icon: <FaPalette className="text-white" />,
     title: "Graphic Design",
     description:
-      "Unlock the power of visual storytelling with our expert graphic design services, tailored to elevate your brand and captivate.",
-    icon: <MdColorLens />, // using imported icon
+      "A market analysis is a quantitative and qualitative assessment of a market. It looks into the size of the market.",
+    iconBgColor: "bg-teal-200",
   },
   {
-    title: "Digital Marketing",
+    icon: <FaChartLine className="text-white" />,
+    title: "Social Media Management",
     description:
-      "Elevate your brand’s online presence with our data-driven digital marketing strategies. From SEO and content marketing.",
-    icon: <FaBullhorn />, // using imported icon
-  },
-  {
-    title: "Web Design",
-    description:
-      "We specialize in creating visually stunning, user-friendly websites that align with your brand identity and deliver an exceptional.",
-    icon: <FaLaptopCode />, // using imported icon
-  },
-  {
-    title: "IT Consulting",
-    description:
-      "IT consulting or information technology consulting refers to the practice of providing advisory and implementation services.",
-    icon: <FaHeadset />, // using imported icon
-  },
-  {
-    title: "Brand Identity",
-    description:
-      "It involves creating a unique and recognizable identity that sets the brand apart from competitors and resonates with the target audience.",
-    icon: <FaFingerprint />, // using imported icon
+      "Our Search Engine Optimization (SEO) service is a comprehensive approach to improving your website's visibility and ranking.",
+    iconBgColor: "bg-green-400",
   },
 ];
-const Services = () => {
-  return (
-    <section className="py-12 bg-white">
-      <div className="container mx-auto px-6">
-        <h2 className="text-2xl font-bold text-center text mb-8 text-teal-400">
-          Our Services
-        </h2>
-        <h3 className="text-3xl font-semibold text-center mb-8">
-          High-impact services for your business
-        </h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {services.map((service, index) => (
-            <div
-              key={index}
-              className="flex flex-col items-left p-6 bg-gray-100 rounded-lg shadow-lg hover:text-white hover:bg-black transition duration-300 cursor-pointer"
-            >
-              <div className="text-3xl mb-4">{service.icon}</div>
-              <h4 className="text-xl  font-semibold mb-2">{service.title}</h4>
-              <p className="active:text-gray-700 text-left ">
-                {service.description}
-              </p>
-              {/* <button className="bg-black text-white hover:bg-white hover:text-black py-2 px-4 rounded-full font-semibold mt-4">
-                Project Here
-              </button> */}
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-};
 
-export default Services;
+const ServiceCard = ({ icon, title, description, iconBgColor }) => (
+  <div className="p-6 bg-white hover:text-white rounded-lg hover:bg-gray-900 cursor-pointer flex flex-col items-center">
+    <div
+      className={`w-16 h-16 flex items-center justify-center rounded-lg ${iconBgColor} mb-4`}
+    >
+      {icon}
+    </div>
+    <h3 className="text-xl font-semibold mb-2">{title}</h3>
+    <p className="text-center">{description}</p>
+  </div>
+);
+
+const Service = () => (
+  <div className="text-center py-10 bg-white">
+    <h2 className="text-lg text-teal-400 font-bold">PREMIUM SERVICE</h2>
+    <h1 className="text-4xl font-bold my-2">That Is The Best Service</h1>
+    <h2 className="text-xl mb-8">Determine The Service You Need</h2>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6">
+      {services.map((service, index) => (
+        <ServiceCard
+          key={index}
+          icon={service.icon}
+          title={service.title}
+          description={service.description}
+          iconBgColor={service.iconBgColor}
+        />
+      ))}
+    </div>
+  </div>
+);
+
+export default Service;
--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -158,149 +158,155 @@ import {
   FaFacebook,
   FaLinkedinIn,
 } from "react-icons/fa";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const ContactSection = () => {
   return (
-    <div>
-      <p className="text-center font-bold text-5xl tracking-wide">
-        Get in Touch
-      </p>
-      <div className="bg-white p-8 md:p-16">
-        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="lg:w-2/3">
-            <h2 className="text-4xl font-bold mb-4">Let's talk</h2>
-            <p className="text-gray-600 mb-8">
-              We collaborate with thousands of creators, entrepreneurs and
-              complete legends.
-            </p>
-            <div className="space-y-6">
-              <div className="flex items-center space-x-4">
-                <FaEnvelope className="text-2xl text-black" />
-                <div>
-                  <h3 className="text-lg font-semibold">Our email</h3>
-                  <p className="text-gray-600">hello@example.com</p>
+    <>
+      <Navbar />
+      <div>
+        <p className="text-center font-bold text-5xl tracking-wide mt-6">
+          Get in Touch
+        </p>
+        <div className="bg-white p-8 md:p-16">
+          <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="lg:w-2/3">
+              <h2 className="text-4xl font-bold mb-4">Let's talk</h2>
+              <p className="text-gray-600 mb-8">
+                We collaborate with thousands of creators, entrepreneurs and
+                complete legends.
+              </p>
+              <div className="space-y-6">
+                <div className="flex items-center space-x-4">
+                  <FaEnvelope className="text-2xl text-black" />
+                  <div>
+                    <h3 className="text-lg font-semibold">Our email</h3>
+                    <p className="text-gray-600">hello@example.com</p>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <FaPhoneAlt className="text-2xl text-black" />
+                  <div>
+                    <h3 className="text-lg font-semibold">Call us</h3>
+                    <p className="text-gray-600">+123 456 7892</p>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <FaMapMarkerAlt className="text-2xl text-black" />
+                  <div>
+                    <h3 className="text-lg font-semibold">Find us</h3>
+                    <p className="text-gray-600">Open Google Maps</p>
+                  </div>
                 </div>
               </div>
-              <div className="flex items-center space-x-4">
-                <FaPhoneAlt className="text-2xl text-black" />
-                <div>
-                  <h3 className="text-lg font-semibold">Call us</h3>
-                  <p className="text-gray-600">+123 456 7892</p>
-                </div>
-              </div>
-              <div className="flex items-center space-x-4">
-                <FaMapMarkerAlt className="text-2xl text-black" />
-                <div>
-                  <h3 className="text-lg font-semibold">Find us</h3>
-                  <p className="text-gray-600">Open Google Maps</p>
-                </div>
+              <div className="flex space-x-4 mt-8">
+                <a href="#">
+                  <FaFacebook className="text-2xl hover:text-gray-600 text-black" />
+                </a>
+                <a href="#">
+                  <FaInstagram className="text-2xl hover:text-gray-600 text-black" />
+                </a>
+                <a href="#">
+                  <FaLinkedinIn className="text-2xl hover:text-gray-600 text-black" />
+                </a>
               </div>
             </div>
-            <div className="flex space-x-4 mt-8">
-              <a href="#">
-                <FaFacebook className="text-2xl hover:text-gray-600 text-black" />
-              </a>
-              <a href="#">
-                <FaInstagram className="text-2xl hover:text-gray-600 text-black" />
-              </a>
-              <a href="#">
-                <FaLinkedinIn className="text-2xl hover:text-gray-600 text-black" />
-              </a>
-            </div>
-          </div>
-          <div>
-            <form className="space-y-4 bg-gray-100 p-6 rounded-xl">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div>
-                  <label
-                    htmlFor="first-name"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    First name
-                  </label>
-                  <input
-                    type="text"
-                    id="first-name"
-                    className="mt-1 p-2 w-full border-b border-gray-300 focus:outline-none focus:border-black bg-transparent"
-                  />
+            <div>
+              <form className="space-y-4 bg-gray-100 p-6 rounded-xl">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      First name
+                    </label>
+                    <input
+                      type="text"
+                      id="first-name"
+                      className="mt-1 p-2 w-full border-b border-gray-300 focus:outline-none focus:border-black bg-transparent"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="last-name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Last name
+                    </label>
+                    <input
+                      type="text"
+                      id="last-name"
+                      className="mt-1 p-2 w-full border-b border-gray-300 focus:outline-none focus:border-black bg-transparent"
+                    />
+                  </div>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      className="mt-1 p-2 w-full border-b border-gray-300 focus:outline-none focus:border-black bg-transparent"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="phone"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Phone
+                    </label>
+                    <input
+                      type="tel"
+                      id="phone"
+                      className="mt-1 p-2 w-full border-b border-gray-300 focus:outline-none focus:border-black bg-transparent"
+                    />
+                  </div>
                 </div>
                 <div>
                   <label
-                    htmlFor="last-name"
+                    htmlFor="message"
                     className="block text-sm font-medium text-gray-700"
                   >
-                    Last name
+                    Message
                   </label>
-                  <input
-                    type="text"
-                    id="last-name"
+                  <textarea
+                    id="message"
                     className="mt-1 p-2 w-full border-b border-gray-300 focus:outline-none focus:border-black bg-transparent"
-                  />
+                    rows="5"
+                  ></textarea>
                 </div>
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="mt-1 p-2 w-full border-b border-gray-300 focus:outline-none focus:border-black bg-transparent"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="phone"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Phone
-                  </label>
-                  <input
-                    type="tel"
-                    id="phone"
-                    className="mt-1 p-2 w-full border-b border-gray-300 focus:outline-none focus:border-black bg-transparent"
-                  />
-                </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-medium text-gray-700"
+                <button
+                  type="submit"
+                  className="bg-black text-white py-3 px-6 rounded-full hover:bg-gray-800"
                 >
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  className="mt-1 p-2 w-full border-b border-gray-300 focus:outline-none focus:border-black bg-transparent"
-                  rows="5"
-                ></textarea>
-              </div>
-              <button
-                type="submit"
-                className="bg-black text-white py-3 px-6 rounded-full hover:bg-gray-800"
-              >
-                Submit Now
-              </button>
-            </form>
+                  Submit Now
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-        <div className="mt-12">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24144.57662277449!2d-74.01152400447836!3d40.706005836892565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a3165ab3e7d%3A0x7647a1b67cb61a4f!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sin!4v1622045795299!5m2!1sen!2sin"
-            width="100%"
-            height="300"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            className="rounded-lg"
-          ></iframe>
+          <div className="mt-12">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24144.57662277449!2d-74.01152400447836!3d40.706005836892565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a3165ab3e7d%3A0x7647a1b67cb61a4f!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sin!4v1622045795299!5m2!1sen!2sin"
+              width="100%"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              className="rounded-lg"
+            ></iframe>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
