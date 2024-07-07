7b4685b2b8217cab2fecae8d100ab3a811abb4f1
chetankhandait <chetankhandait16@gmail.com>
2024-07-07 13:45:10 +0530

    Testinomial

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -8,69 +8,159 @@ const Testimonial = () => {
   return (
     
 
-    <div class="mt-6 font-[sans-serif] py-16">
-            <div class="max-w-4xl mx-auto relative">
-                <div class="text-center">
-                    <h2 class="text-gray-800 text-3xl font-extrabold">What our happy client say</h2>
-                    <p class="text-sm text-gray-800 mt-6 leading-relaxed">Veniam proident aute magna anim excepteur et ex consectetur velit ullamco veniam minim aute sit. Elit occaecat officia et laboris Lorem minim. Officia do aliqua adipisicing ullamco in.</p>
-                </div>
+    // <div className="mt-6 font-[sans-serif] py-16">
+    //         <div className="max-w-4xl mx-auto relative">
+    //             <div className="text-center">
+    //                 <h2 className="text-gray-800 text-3xl font-extrabold">What our happy client say</h2>
+    //                 <p className="text-sm text-gray-800 mt-6 leading-relaxed">Veniam proident aute magna anim excepteur et ex consectetur velit ullamco veniam minim aute sit. Elit occaecat officia et laboris Lorem minim. Officia do aliqua adipisicing ullamco in.</p>
+    //             </div>
 
-                <div class="max-w-xl mt-16 mx-auto">
-                    <div class="flex flex-col items-center text-center">
-                        <img src="https://readymadeui.com/team-1.webp" class="w-28 h-28 rounded-full shadow-[0_2px_22px_-4px_rgba(93,96,127,0.6)] border-2 border-white"
-                        loading='lazy'
-                        />
-                        <div class="mt-4">
-                            <h4 class="text-gray-800 text-base font-extrabold">John Doe</h4>
-                            <p class="text-xs text-gray-500 mt-1">CEO, Company</p>
-                        </div>
+    //             <div className="max-w-xl mt-16 mx-auto">
+    //                 <div className="flex flex-col items-center text-center">
+    //                     <img src="https://readymadeui.com/team-1.webp" className="w-28 h-28 rounded-full shadow-[0_2px_22px_-4px_rgba(93,96,127,0.6)] border-2 border-white"
+    //                     loading='lazy'
+    //                     />
+    //                     <div className="mt-4">
+    //                         <h4 className="text-gray-800 text-base font-extrabold">John Doe</h4>
+    //                         <p className="text-xs text-gray-500 mt-1">CEO, Company</p>
+    //                     </div>
+    //                 </div>
+
+    //                 <div className="mt-6 text-center">
+    //                     <p className="text-sm leading-relaxed">The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive, and the delivery was impressively prompt.</p>
+    //                 </div>
+
+    //                 <div className="flex justify-center space-x-1.5 mt-4">
+    //                     <svg className="w-[18px] fill-[#facc15]" viewBox="0 0 14 13" fill="none"
+    //                         xmlns="http://www.w3.org/2000/svg">
+    //                         <path
+    //                             d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+    //                     </svg>
+    //                     <svg className="w-[18px] fill-[#facc15]" viewBox="0 0 14 13" fill="none"
+    //                         xmlns="http://www.w3.org/2000/svg">
+    //                         <path
+    //                             d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+    //                     </svg>
+    //                     <svg className="w-[18px] fill-[#facc15]" viewBox="0 0 14 13" fill="none"
+    //                         xmlns="http://www.w3.org/2000/svg">
+    //                         <path
+    //                             d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+    //                     </svg>
+    //                     <svg className="w-[18px] fill-[#CED5D8]" viewBox="0 0 14 13" fill="none"
+    //                         xmlns="http://www.w3.org/2000/svg">
+    //                         <path
+    //                             d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+    //                     </svg>
+    //                     <svg className="w-[18px] fill-[#CED5D8]" viewBox="0 0 14 13" fill="none"
+    //                         xmlns="http://www.w3.org/2000/svg">
+    //                         <path
+    //                             d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+    //                     </svg>
+    //                 </div>
+    //             </div>
+
+    //             <div className="bg-gray-300 w-10 h-10 grid items-center justify-center rounded-full rotate-90 shrink-0 cursor-pointer absolute left-0 top-0 bottom-0 my-auto">
+    //                 <svg xmlns="http://www.w3.org/2000/svg" className="w-3 fill-gray-800 inline" viewBox="0 0 24 24">
+    //                     <path fill-rule="evenodd" d="M11.99997 18.1669a2.38 2.38 0 0 1-1.68266-.69733l-9.52-9.52a2.38 2.38 0 1 1 3.36532-3.36532l7.83734 7.83734 7.83734-7.83734a2.38 2.38 0 1 1 3.36532 3.36532l-9.52 9.52a2.38 2.38 0 0 1-1.68266.69734z" clip-rule="evenodd" data-original="#000000"></path>
+    //                 </svg>
+    //             </div>
+    //             <div className="bg-gray-800 w-10 h-10 grid items-center justify-center rounded-full -rotate-90 shrink-0 cursor-pointer absolute right-0 top-0 bottom-0 my-auto">
+    //                 <svg xmlns="http://www.w3.org/2000/svg" className="w-3 fill-[#fff] inline" viewBox="0 0 24 24">
+    //                     <path fill-rule="evenodd" d="M11.99997 18.1669a2.38 2.38 0 0 1-1.68266-.69733l-9.52-9.52a2.38 2.38 0 1 1 3.36532-3.36532l7.83734 7.83734 7.83734-7.83734a2.38 2.38 0 1 1 3.36532 3.36532l-9.52 9.52a2.38 2.38 0 0 1-1.68266.69734z" clip-rule="evenodd" data-original="#000000"></path>
+    //                 </svg>
+    //             </div>
+    //         </div>
+    //     </div>
+    <section className="relative flex">
+    <div className="min-h-screen bg-white dark:bg-gray-900 md:w-3/4"></div>
+    <div className="min-h-screen bg-blue-500 md:w-2/5"></div>
+
+    <div className="flex flex-col justify-center w-full min-h-screen px-4 py-10 md:fixed md:mx-24">
+        <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+            What our <span className="text-blue-500">customers</span> <br/> are saying
+        </h1>
+
+        <div className="grid w-full grid-cols-1 gap-8 mt-8 2xl:grid-cols-4 lg:mt-16 md:grid-cols-2 lg:grid-cols-3">
+            <div className="w-full p-8 bg-white rounded-md shadow-lg dark:bg-gray-800">
+                <p className="leading-loose text-gray-500 dark:text-gray-400">
+                    “ Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ea tempora dolores qui eius pariatur
+                    odit ad voluptas iste, cum accusantium beatae tempore quasi nesciunt distinctio. ”
+                </p>
+
+                <div className="flex items-center mt-6 -mx-2">
+                    <img className="object-cover mx-2 rounded-full w-14 h-14" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt=""/>
+
+                    <div className="mx-2">
+                        <h1 className="font-semibold text-gray-800 dark:text-white">Robbert</h1>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">CTO, Robert Consultency</span>
                     </div>
+                </div>
+            </div>
 
-                    <div class="mt-6 text-center">
-                        <p class="text-sm leading-relaxed">The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive, and the delivery was impressively prompt.</p>
-                    </div>
+            <div className="w-full p-8 bg-white rounded-md shadow-lg dark:bg-gray-800">
+                <p className="leading-loose text-gray-500 dark:text-gray-400">
+                    “ Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ea tempora dolores qui eius pariatur
+                    odit ad voluptas iste, cum accusantium beatae tempore quasi nesciunt distinctio. ”
+                </p>
 
-                    <div class="flex justify-center space-x-1.5 mt-4">
-                        <svg class="w-[18px] fill-[#facc15]" viewBox="0 0 14 13" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                        </svg>
-                        <svg class="w-[18px] fill-[#facc15]" viewBox="0 0 14 13" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                        </svg>
-                        <svg class="w-[18px] fill-[#facc15]" viewBox="0 0 14 13" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                        </svg>
-                        <svg class="w-[18px] fill-[#CED5D8]" viewBox="0 0 14 13" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                        </svg>
-                        <svg class="w-[18px] fill-[#CED5D8]" viewBox="0 0 14 13" fill="none"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                        </svg>
+                <div className="flex items-center mt-6 -mx-2">
+                    <img className="object-cover mx-2 rounded-full w-14 h-14" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=761&q=80" alt=""/>
+
+                    <div className="mx-2">
+                        <h1 className="font-semibold text-gray-800 dark:text-white">Jeny Doe</h1>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">CEO, Jeny Consultency</span>
                     </div>
                 </div>
+            </div>
 
-                <div class="bg-gray-300 w-10 h-10 grid items-center justify-center rounded-full rotate-90 shrink-0 cursor-pointer absolute left-0 top-0 bottom-0 my-auto">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-3 fill-gray-800 inline" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M11.99997 18.1669a2.38 2.38 0 0 1-1.68266-.69733l-9.52-9.52a2.38 2.38 0 1 1 3.36532-3.36532l7.83734 7.83734 7.83734-7.83734a2.38 2.38 0 1 1 3.36532 3.36532l-9.52 9.52a2.38 2.38 0 0 1-1.68266.69734z" clip-rule="evenodd" data-original="#000000"></path>
-                    </svg>
+            <div className="w-full p-8 bg-white rounded-md shadow-lg dark:bg-gray-800 md:hidden lg:block">
+                <p className="leading-loose text-gray-500 dark:text-gray-400">
+                    “ Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ea tempora dolores qui eius pariatur
+                    odit ad voluptas iste, cum accusantium beatae tempore quasi nesciunt distinctio. ”
+                </p>
+
+                <div className="flex items-center mt-6 -mx-2">
+                    <img className="object-cover mx-2 rounded-full w-14 h-14" src="https://images.unsplash.com/photo-1499470932971-a90681ce8530?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
+
+                    <div className="mx-2">
+                        <h1 className="font-semibold text-gray-800 dark:text-white">Mia Brown</h1>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">Marketing Manager at Stech</span>
+                    </div>
                 </div>
-                <div class="bg-gray-800 w-10 h-10 grid items-center justify-center rounded-full -rotate-90 shrink-0 cursor-pointer absolute right-0 top-0 bottom-0 my-auto">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-3 fill-[#fff] inline" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M11.99997 18.1669a2.38 2.38 0 0 1-1.68266-.69733l-9.52-9.52a2.38 2.38 0 1 1 3.36532-3.36532l7.83734 7.83734 7.83734-7.83734a2.38 2.38 0 1 1 3.36532 3.36532l-9.52 9.52a2.38 2.38 0 0 1-1.68266.69734z" clip-rule="evenodd" data-original="#000000"></path>
-                    </svg>
+            </div>
+
+            <div className="w-full p-8 bg-white rounded-md shadow-lg dark:bg-gray-800 md:hidden 2xl:block">
+                <p className="leading-loose text-gray-500 dark:text-gray-400">
+                    “ Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ea tempora dolores qui eius pariatur
+                    odit ad voluptas iste, cum accusantium beatae tempore quasi nesciunt distinctio. ”
+                </p>
+
+                <div className="flex items-center mt-6 -mx-2">
+                    <img className="object-cover mx-2 rounded-full w-14 h-14" src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
+
+                    <div className="mx-2">
+                        <h1 className="font-semibold text-gray-800 dark:text-white">Lead Designer</h1>
+                        <span className="text-sm text-gray-500 dark:text-gray-400">Developer at Stech</span>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div className="items-center hidden mt-12 md:flex">
+            <button title="left arrow" className="p-2 text-gray-800 transition-colors duration-300 border rounded-full rtl:-scale-x-100 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 hover:bg-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                </svg>
+            </button>
+
+            <button title="right arrow" className="p-2 text-gray-800 transition-colors duration-300 border rounded-full rtl:-scale-x-100 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 lg:mx-6 hover:bg-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+                </svg>
+            </button>
+        </div>
+    </div>
+</section>
   );
 };
 
