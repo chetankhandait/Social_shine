3f7353e9bfd0fffcd1fe5148754a84425dcc153a
chetankhandait <chetankhandait16@gmail.com>
2024-07-09 18:06:48 +0530

    add iamge in the about us

--- src/Pages/BuisnessSection.jsx
+++ src/Pages/BuisnessSection.jsx
@@ -118,7 +118,7 @@ const BuisnessSection = () => {
           <section className="grid md:grid-cols-2 gap-8 mb-16">
             <div>
               <img
-                src="/team.svg"
+                src="/about-us-page-animate.svg"
                 alt="About Us"
                 loading="lazy"
                 className="rounded-lg"
@@ -144,110 +144,9 @@ const BuisnessSection = () => {
             </div>
           </section>
 
-          {/* <section className="text-center mb-16">
-            <h2 className="text-3xl font-semibold mb-8">What We Do</h2>
-            <div className="flex flex-wrap justify-center gap-8">
-              <div className="w-60 p-4 bg-gray-800 rounded-lg shadow-md hover:shadow-lg transition-shadow">
-                <FaBullhorn className="text-4xl text-blue-500 mb-4 mx-auto" />
-                <h3 className="text-xl font-medium mb-2">
-                  Social Media Strategy
-                </h3>
-                <p className="text-gray-400">
-                  We craft customized strategies that align with your business
-                  goals and resonate with your audience.
-                </p>
-              </div>
-              <div className="w-60 p-4 bg-gray-800 rounded-lg shadow-md hover:shadow-lg transition-shadow">
-                <FaUsers className="text-4xl text-blue-500 mb-4 mx-auto" />
-                <h3 className="text-xl font-medium mb-2">
-                  Influencer Marketing
-                </h3>
-                <p className="text-gray-400">
-                  Connect with influencers to expand your reach and build
-                  authentic relationships with your audience.
-                </p>
-              </div>
-              <div className="w-60 p-4 bg-gray-800 rounded-lg shadow-md hover:shadow-lg transition-shadow">
-                <FaAward className="text-4xl text-blue-500 mb-4 mx-auto" />
-                <h3 className="text-xl font-medium mb-2">Content Creation</h3>
-                <p className="text-gray-400">
-                  Our team creates engaging content that captures your brand’s
-                  voice and keeps your audience hooked.
-                </p>
-              </div>
-            </div>
-          </section>
-
-          <section className="text-center mb-16">
-            <h2 className="text-3xl font-semibold mb-8">Our Achievements</h2>
-            <div className="flex flex-wrap justify-center gap-8">
-              <div className="w-60 p-4 bg-gray-800 rounded-lg shadow-md">
-                <FaAward className="text-4xl text-blue-500 mb-4 mx-auto" />
-                <h3 className="text-xl font-medium mb-2">
-                  Award-Winning Campaigns
-                </h3>
-                <p className="text-gray-400">
-                  Recognized for our innovative campaigns that deliver
-                  outstanding results.
-                </p>
-              </div>
-              <div className="w-60 p-4 bg-gray-800 rounded-lg shadow-md">
-                <FaUsers className="text-4xl text-blue-500 mb-4 mx-auto" />
-                <h3 className="text-xl font-medium mb-2">Satisfied Clients</h3>
-                <p className="text-gray-400">
-                  Our clients’ success stories and testimonials speak volumes
-                  about our dedication and expertise.
-                </p>
-              </div>
-            </div>
-          </section> */}
+           
         </div>
-        {/* <section className="bg-black text-center py-16 px-4">
-        <div className="container mx-auto">
-          <h2 className="text-2xl font-bold mb-8">
-            THESE ARE THE BELIEFS{" "}
-            <span className="text-blue-500">WE STAND FOR</span>
-          </h2>
-          <div className="grid grid-cols-2 md:grid-cols-3">
-            <div className="belief-item mx-4 my-4">
-              <div className="circle bg-blue-600 flex items-center justify-center rounded-full w-20 h-20 mb-4 mx-auto">
-                <span className="text-3xl font-bold text-white">1</span>
-              </div>
-              <h3 className="font-semibold">FOCUSED ON RESULTS</h3>
-            </div>
-            <div className="belief-item mx-4 my-4">
-              <div className="circle bg-blue-600 flex items-center justify-center rounded-full w-20 h-20 mb-4 mx-auto">
-                <span className="text-3xl font-bold text-white">2</span>
-              </div>
-              <h3 className="font-semibold">RESPECTFUL & FAIR</h3>
-            </div>
-            <div className="belief-item mx-4 my-4">
-              <div className="circle bg-blue-600 flex items-center justify-center rounded-full w-20 h-20 mb-4 mx-auto">
-                <span className="text-3xl font-bold text-white">3</span>
-              </div>
-              <h3 className="font-semibold">OPEN TO CHANGE</h3>
-            </div>
-            <div className="belief-item mx-4 my-4">
-              <div className="circle bg-blue-600 flex items-center justify-center rounded-full w-20 h-20 mb-4 mx-auto">
-                <span className="text-3xl font-bold text-white">4</span>
-              </div>
-              <h3 className="font-semibold">COMMITTED TO LEARNING</h3>
-            </div>
-            <div className="belief-item mx-4 my-4">
-              <div className="circle bg-blue-600 flex items-center justify-center rounded-full w-20 h-20 mb-4 mx-auto">
-                <span className="text-3xl font-bold text-white">5</span>
-              </div>
-              <h3 className="font-semibold">BUSINESS FOR GOOD</h3>
-            </div>
-            <div className="belief-item mx-4 my-4">
-              <div className="circle bg-blue-600 flex items-center justify-center rounded-full w-20 h-20 mb-4 mx-auto">
-                <span className="text-3xl font-bold text-white">6</span>
-              </div>
-              <h3 className="font-semibold">BUSINESS FOR GOOD</h3>
-            </div>
-          </div>
-        </div>
-      </section> */}
+        
         <Whatmakesdifferent />
       </div>
     </>
