c080e6b8694644938a6fd4706fffa9cf11097472
chetankhandait <chetankhandait16@gmail.com>
2024-07-09 09:30:11 +0530

    meta ta g and services

--- src/SEO/MetaTag.jsx
+++ src/SEO/MetaTag.jsx
@@ -7,8 +7,8 @@ const MetaTags = () => {
       <meta name="google-site-verification" content="wdfWM8KyMmHfdyZ3_JrJbzjRghROkqQiawy3vNkWzLc" />
       <link rel="icon" type="image/svg+xml" href="/logo.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Explore how Social Shine, a leading social media marketing agency, enhances online presence through expert website and app development services. Learn SEO strategies and meta description best practices to optimize your digital strategy." />
-      <title>Social Shine: Expert Social Media Marketing, Website, and App Development Services</title>
+      <meta name="description" content="Social Shine, the top social media marketing agency, enhances your brand's potential with expert website and app development, SEO strategies boost your online presence!" />
+      <title>Social Shine: Expert Social Media Marketing,Website and App Development</title>
     </Helmet>
   );
 }
